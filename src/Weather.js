13062487b009d070b1f5cdff4d5df903aec4faf8
youming16 <tanyouming1@gmail.com>
2023-03-04 05:46:40 +1100

    feature updated

--- src/Weather.js
+++ src/Weather.js
@@ -1,31 +1,52 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+
+
 const Weather = () => {
-  const [weatherData, setWeatherData] = useState(null);
-  const [city, setCity] = useState("");
+    const [weatherData, setWeatherData] = useState(null);
+    const [city, setCity] = useState("");
+    
 
-  const getWeatherData = async () => {
-    const apiKey = "0d12ae117ff685cda29153295641d917";
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
-    );
-    setWeatherData(response.data);
-  };
+    const getWeatherData = async () => {
+        const apiKey = "0d12ae117ff685cda29153295641d917";
+        const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        );
+        setWeatherData(response.data);
+    };
 
-  return (
-    <div>
-      <input type="text" onChange={(e) => setCity(e.target.value)} />
-      <button onClick={getWeatherData}>Get Weather</button>
-      {weatherData && (
+    const cities = [
+        { id: 1, name: 'Sydney' },
+        { id: 2, name: 'Melbourne' },
+        { id: 3, name: 'Brisbane' },
+        { id: 4, name: 'Perth' },
+        { id: 5, name: 'Adelaide' },
+        { id: 6, name: 'Gold Coast' },
+        // Add more cities as needed
+    ];
+
+  
+    // Render the selection
+    return (
         <div>
-          <h1>{weatherData.name}</h1>
-          <p>{weatherData.main.temp}°F</p>
-          <p>{weatherData.weather[0].description}</p>
+            <label htmlFor="city">Choose a city:</label>
+            <select id="city" name="city" onChange={(event) => setCity(event.target.value)}>
+                {cities.map(city => (
+                    <option key={city.id} value={city.name}>{city.name}</option>
+                ))}
+            </select>
+            <button onClick={getWeatherData}>Get Weather</button>
+            {weatherData && (
+                <div>
+                <h1>{weatherData.name}</h1>
+                <p>{weatherData.main.temp}°C</p>
+                <p>{weatherData.weather[0].description}</p>
+                </div>
+            )}
+        
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Weather;
